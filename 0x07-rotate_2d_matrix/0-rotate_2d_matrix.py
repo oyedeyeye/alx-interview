787cd91641513c957e91fedff86674ca0475f639
Hezekiah Oyemade <oyedey@gmail.com>
2023-08-18 02:00:25 +0100

    rotate 2D Matrix Algorithm

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -19,16 +19,16 @@ def rotate_2d_matrix(matrix):
     if not all(map(lambda l: len(l) == cols, matrix)):
         return
 
-    c = 0
-    r = rows - 1
+    x = 0
+    y = rows - 1
 
     for i in range(cols * rows):
         if i % rows == 0:
             matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+        if y == -1:
+            y = rows - 1
+            x += 1
+        matrix[-1].append(matrix[y][x])
+        if x == cols - 1 and y >= -1:
+            matrix.pop(y)
+        y -= 1
