d54f928edebdbdc1280e4abd447bbd9236bf4f9c
Hezekiah Oyemade <oyedey@gmail.com>
2023-09-28 23:30:41 +0100

    refactored code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,21 +6,20 @@ Prime Number Game
 
 def check_prime_number(n):
     """ check if n is a prime number """
-    for i in range(2, int(n ** 0.5) + 1):
-        if not n % i:
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False
         return True
 
 
-def add_prime_number(n, primes):
-    """Adds primes to list"""
-    last_prime_number = primes[-1]
-    if n > last_prime_number:
-        for i in range(last_prime_number + 1, n + 1):
-            if check_prime_number(i):
-                primes.append(i)
-            else:
-                primes.append(0)
+def next_prime_number(start, numbers):
+    """checks next primes in the list"""
+    for num in range(start + 1, len(numbers)):
+        if numbers[num] and check_prime_number(num):
+            return num
+    return None
 
 
 def isWinner(x, nums):
@@ -33,23 +32,32 @@ def isWinner(x, nums):
     if x >= 10000 or max(nums) >= 10000:
         return None
 
-    score = {"Maria": 0, "Ben": 0}
-    primes = [0, 0, 2]
-    add_prime_number(max(nums), primes)
+    maria_wins = 0
+    ben_wins = 0
 
-    for round in range(x):
-        _tot = sum((i != 0 and i <= nums[round])
-                   for i in primes[:nums[round] + 1])
-        if (_tot % 2):
-            winner = "Maria"
-        else:
-            winner = "Ben"
-        if winner:
-            score[winner] += 1
+    for n in nums:
+        numbers = [True] * (n + 1)
+        numbers[0] = numbers[1] = False
+        turn_maria = True
 
-    if score["Maria"] > score["Ben"]:
+        while True:
+            prime = next_prime_number(1, numbers)
+            if prime is None:
+                break
+
+            for multiple in range(prime, len(numbers), prime):
+                numbers[multiple] = False
+
+            if turn_maria:
+                maria_wins += 1
+            else:
+                ben_wins += 1
+
+            turn_maria = not turn_maria
+
+    if maria_wins > ben_wins:
         return "Maria"
-    elif score["Ben"] > score["Maria"]:
+    elif ben_wins > maria_wins:
         return "Ben"
-
-    return None
+    else:
+        return None
