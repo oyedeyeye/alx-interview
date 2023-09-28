44300e6b8e391dc130256e0d83efaa4b5d1a0b3e
Hezekiah Oyemade <oyedey@gmail.com>
2023-09-28 23:53:46 +0100

    refactored code to handle edge cases

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
 Prime Number Game
-"""
+
 
 
 def check_prime_number(n):
-    """ check if n is a prime number """
+    " check if n is a prime number
     if n <= 1:
         return False
     for i in range(2, int(n**0.5) + 1):
@@ -15,7 +15,7 @@ def check_prime_number(n):
 
 
 def next_prime_number(start, numbers):
-    """checks next primes in the list"""
+    "checks next primes in the list
     for num in range(start + 1, len(numbers)):
         if numbers[num] and check_prime_number(num):
             return num
@@ -23,15 +23,12 @@ def next_prime_number(start, numbers):
 
 
 def isWinner(x, nums):
-    """
+    "
     where x is the number of rounds and nums is an array of n
     Return: name of the player that won the most rounds
     If the winner cannot be determined, return None
     You can assume n and x will not be larger than 10000
-    """
-    if x >= 10000 or max(nums) >= 10000:
-        return None
-
+    "
     maria_wins = 0
     ben_wins = 0
 
@@ -54,6 +51,40 @@ def isWinner(x, nums):
                 ben_wins += 1
 
             turn_maria = not turn_maria
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
+"""
+
+
+def isWinner(x, nums):
+    def sieve_of_eratosthenes(n):
+        primes = [True] * (n + 1)
+        p = 2
+        while p**2 <= n:
+            if primes[p]:
+                for i in range(p**2, n + 1, p):
+                    primes[i] = False
+            p += 1
+        return [i for i in range(2, len(primes)) if primes[i]]
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n <= 1:
+            ben_wins += 1
+            continue
+        primes = sieve_of_eratosthenes(n)
+        turns = len(primes)
+        if turns % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
